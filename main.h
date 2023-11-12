b5c01ca4983f3620c9680fd1be55deff694890b3
Veronica_Ogalo <veronicaogalo@example.com>
2023-11-12 10:02:11 -0800

    main.h

--- main.h
+++ main.h
@@ -45,3 +45,652 @@ int _putchar(char c);
 int _printf(const char *format, ...);
 
 #endif
+
+file: print_HEX.c
+
+#include "main.h"
+
+/**
+ * print_HEX - prints an hexgecimal number.
+ * @val: arguments.
+ * Return: counter.
+ */
+int print_HEX(va_list val)
+{
+	int i;
+	int *array;
+	int counter = 0;
+	unsigned int num = va_arg(val, unsigned int);
+	unsigned int tem = num;
+
+	while (num / 16 != 0)
+	{
+		num /= 16;
+		counter++;
+	}
+	counter++;
+	array = malloc(counter * sizeof(int));
+
+	for (i = 0; i < counter; i++)
+	{
+		array[i] = tem % 16;
+		tem /= 16;
+	}
+	for (i = counter - 1; i >= 0; i--)
+	{
+		if (array[i] > 9)
+			array[i] = array[i] + 7;
+		_putchar(array[i] + '0');
+	}
+	free(array);
+	return (counter);
+}
+
+file: print_HEX_extra.c
+
+#include "main.h"
+
+/**
+ * print_HEX_extra - prints an hexgecimal number.
+ * @num: number to print.
+ * Return: counter.
+ */
+int print_HEX_extra(unsigned int num)
+{
+	int i;
+	int *array;
+	int counter = 0;
+	unsigned int tem = num;
+
+	while (num / 16 != 0)
+	{
+		num /= 16;
+		counter++;
+	}
+	counter++;
+	array = malloc(counter * sizeof(int));
+
+	for (i = 0; i < counter; i++)
+	{
+		array[i] = tem % 16;
+		tem /= 16;
+	}
+	for (i = counter - 1; i >= 0; i--)
+	{
+		if (array[i] > 9)
+			array[i] = array[i] + 7;
+		_putchar(array[i] + '0');
+	}
+	free(array);
+	return (counter);
+}
+
+file: print_bin.c
+
+#include "main.h"
+
+/**
+ * print_bin - prints binary number.
+ * @val: parameter.
+ * Return: integer
+ */
+int print_bin(va_list val)
+{
+	int flag = 0;
+	int cont = 0;
+	int i, a = 1, b;
+	unsigned int num = va_arg(val, unsigned int);
+	unsigned int p;
+
+	for (i = 0; i < 32; i++)
+	{
+		p = ((a << (31 - i)) & num);
+		if (p >> (31 - i))
+			flag = 1;
+		if (flag)
+		{
+			b = p >> (31 - i);
+			_putchar(b + 48);
+			cont++;
+		}
+	}
+	if (cont == 0)
+	{
+		cont++;
+		_putchar('0');
+	}
+	return (cont);
+}
+
+file: print_c.c
+
+#include "main.h"
+
+/**
+ * print_c - prints a character.
+ * @val: arguments.
+ * Return: integer.
+ */
+int print_c(va_list val)
+{
+	char s;
+
+	s = va_arg(val, int);
+	_putchar(s);
+	return (1);
+}
+
+file: print_exc_string.c
+
+#include "main.h"
+/**
+ * print_exc_string - print exclusuives string.
+ * @val: parameter.
+ * Return: integer.
+ */
+
+int print_exc_string(va_list val)
+{
+	char *s;
+	int i, length = 0;
+	int value;
+
+	s = va_arg(val, char *);
+	if (s == NULL)
+		s = "(null)";
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] < 32 || s[i] >= 127)
+		{
+			_putchar('\\');
+			_putchar('x');
+			length = length + 2;
+			value = s[i];
+			if (value < 16)
+			{
+				_putchar('0');
+				length++;
+			}
+			length = length + print_HEX_extra(value);
+		}
+		else
+		{
+			_putchar(s[i]);
+			length++;
+		}
+	}
+	return (length);
+}
+
+file: print_hex.c
+
+#include "main.h"
+
+/**
+ * print_hex - prints an hexgecimal number.
+ * @val: arguments.
+ * Return: counter.
+ */
+int print_hex(va_list val)
+{
+	int i;
+	int *array;
+	int counter = 0;
+	unsigned int num = va_arg(val, unsigned int);
+	unsigned int tem = num;
+
+	while (num / 16 != 0)
+	{
+		num /= 16;
+		counter++;
+	}
+	counter++;
+	array = malloc(counter * sizeof(int));
+
+	for (i = 0; i < counter; i++)
+	{
+		array[i] = tem % 16;
+		tem /= 16;
+	}
+	for (i = counter - 1; i >= 0; i--)
+	{
+		if (array[i] > 9)
+			array[i] = array[i] + 39;
+		_putchar(array[i] + '0');
+	}
+	free(array);
+	return (counter);
+}
+
+file: print_hex_extra.c
+
+#include "main.h"
+
+/**
+ * print_hex_extra - prints an hexgecimal number.
+ * @num: arguments.
+ * Return: counter.
+ */
+int print_hex_extra(unsigned long int num)
+{
+	long int i;
+	long int *array;
+	long int counter = 0;
+	unsigned long int temp = num;
+
+	while (num / 16 != 0)
+	{
+		num /= 16;
+		counter++;
+	}
+	counter++;
+	array = malloc(counter * sizeof(long int));
+
+	for (i = 0; i < counter; i++)
+	{
+		array[i] = temp % 16;
+		temp = temp / 16;
+	}
+	for (i = counter - 1; i >= 0; i--)
+	{
+		if (array[i] > 9)
+			array[i] = array[i] + 39;
+		_putchar(array[i] + '0');
+	}
+	free(array);
+	return (counter);
+}
+
+file: print_int.c
+
+#include "main.h"
+/**
+ * print_i - prints integer
+ * @args: argument to print
+ * Return: integer
+ */
+int print_i(va_list args)
+{
+	int n = va_arg(args, int);
+	int num, last = n % 10, digit, exp = 1;
+	int  i = 1;
+
+	n = n / 10;
+	num = n;
+
+	if (last < 0)
+	{
+		_putchar('-');
+		num = -num;
+		n = -n;
+		last = -last;
+		i++;
+	}
+	if (num > 0)
+	{
+		while (num / 10 != 0)
+		{
+			exp = exp * 10;
+			num = num / 10;
+		}
+		num = n;
+		while (exp > 0)
+		{
+			digit = num / exp;
+			_putchar(digit + '0');
+			num = num - (digit * exp);
+			exp = exp / 10;
+			i++;
+		}
+	}
+	_putchar(last + '0');
+
+	return (i);
+}
+
+/**
+ * print_d - prints decimal
+ * @args: argument to print
+ * Return: integer
+ */
+
+int print_d(va_list args)
+{
+	int n = va_arg(args, int);
+	int num, last = n % 10, digit;
+	int  i = 1;
+	int exp = 1;
+
+	n = n / 10;
+	num = n;
+
+	if (last < 0)
+	{
+		_putchar('-');
+		num = -num;
+		n = -n;
+		last = -last;
+		i++;
+	}
+	if (num > 0)
+	{
+		while (num / 10 != 0)
+		{
+			exp = exp * 10;
+			num = num / 10;
+		}
+		num = n;
+		while (exp > 0)
+		{
+			digit = num / exp;
+			_putchar(digit + '0');
+			num = num - (digit * exp);
+			exp = exp / 10;
+			i++;
+		}
+	}
+	_putchar(last + '0');
+
+	return (i);
+}
+
+file: print_oct.c
+
+#include "main.h"
+
+/**
+ * print_oct - prints an octal number.
+ * @val: arguments.
+ * Return: counter.
+ */
+int print_oct(va_list val)
+{
+	int i;
+	int *array;
+	int counter = 0;
+	unsigned int num = va_arg(val, unsigned int);
+	unsigned int tem = num;
+
+	while (num / 8 != 0)
+	{
+		num /= 8;
+		counter++;
+	}
+	counter++;
+	array = malloc(counter * sizeof(int));
+
+	for (i = 0; i < counter; i++)
+	{
+		array[i] = tem % 8;
+		tem /= 8;
+	}
+	for (i = counter - 1; i >= 0; i--)
+	{
+		_putchar(array[i] + '0');
+	}
+	free(array);
+	return (counter);
+}
+
+file: print_pointer.c
+
+#include "main.h"
+
+/**
+ * print_pointer - prints an hexgecimal number.
+ * @val: arguments.
+ * Return: counter.
+ */
+int print_pointer(va_list val)
+{
+	void *p;
+	char *s = "(nil)";
+	long int x;
+	int y;
+	int i;
+
+	p = va_arg(val, void*);
+	if (p == NULL)
+	{
+		for (i = 0; s[i] != '\0'; i++)
+		{
+			_putchar(s[i]);
+		}
+		return (i);
+	}
+
+	x = (unsigned long int)p;
+	_putchar('0');
+	_putchar('x');
+	y = print_hex_extra(x);
+	return (y + 2);
+}
+
+file: print_rot13.c
+
+#include "main.h"
+/**
+ * print_rot13 - convert to rot13
+ * @args: printf arguments
+ * Return: counter
+ *
+ */
+int print_rot13(va_list args)
+{
+	int i, j, counter = 0;
+	int k = 0;
+	char *s = va_arg(args, char*);
+	char al[] = {"abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ"};
+	char bl[] = {"nopqrstuvwxyzabcdefghijklmNOPQRSTUVWXYZABCDEFGHIJKLM"};
+
+	if (s == NULL)
+		s = "(null)";
+	for (i = 0; s[i]; i++)
+	{
+		k = 0;
+		for (j = 0; al[j] && !k; j++)
+		{
+			if (s[i] == al[j])
+			{
+				_putchar(bl[j]);
+				counter++;
+				k = 1;
+			}
+		}
+		if (!k)
+		{
+			_putchar(s[i]);
+			counter++;
+		}
+	}
+	return (counter);
+}
+
+file: print_srev.c
+
+#include "main.h"
+
+/**
+ * print_revs - function that prints a str in reverse
+ * @args: type struct va_arg where is allocated printf arguments
+ *
+ * Return: the string
+ */
+int print_revs(va_list args)
+{
+
+	char *s = va_arg(args, char*);
+	int i;
+	int j = 0;
+
+	if (s == NULL)
+		s = "(null)";
+	while (s[j] != '\0')
+		j++;
+	for (i = j - 1; i >= 0; i--)
+		_putchar(s[i]);
+	return (j);
+}
+
+file: print_str.c
+
+#include "main.h"
+
+/**
+ * _strlen - finds the lenght of a string.
+ * @s: string
+ * Return: integer.
+ */
+
+int _strlen(char *s)
+{
+	int c;
+
+	for (c = 0; s[c] != 0; c++)
+		;
+	return (c);
+
+}
+
+/**
+ * _strlenc - finds length of a string but for constant characters
+ * @s: string
+ * Return: integer
+ */
+
+int _strlenc(const char *s)
+{
+	int c;
+
+	for (c = 0; s[c] != 0; c++)
+		;
+	return (c);
+}
+
+file: print_string.c
+
+#include "main.h"
+/**
+ * print_s - print a string.
+ * @val: argumen t.
+ * Return: the length of the string.
+ */
+
+int print_s(va_list val)
+{
+	char *s;
+	int i, len;
+
+	s = va_arg(val, char *);
+	if (s == NULL)
+	{
+		s = "(null)";
+		len = _strlen(s);
+		for (i = 0; i < len; i++)
+			_putchar(s[i]);
+		return (len);
+	}
+	else
+	{
+		len = _strlen(s);
+		for (i = 0; i < len; i++)
+			_putchar(s[i]);
+		return (len);
+	}
+}
+
+file: print_uns.c
+
+#include "main.h"
+/**
+ * print_unsigned - prints integer
+ * @args: argument to print
+ * Return: integer
+ */
+int print_unsigned(va_list args)
+{
+	unsigned int n = va_arg(args, unsigned int);
+	int num, last = n % 10, digit, exp = 1;
+	int  i = 1;
+
+	n = n / 10;
+	num = n;
+
+	if (last < 0)
+	{
+		_putchar('-');
+		num = -num;
+		n = -n;
+		last = -last;
+		i++;
+	}
+	if (num > 0)
+	{
+		while (num / 10 != 0)
+		{
+			exp = exp * 10;
+			num = num / 10;
+		}
+		num = n;
+		while (exp > 0)
+		{
+			digit = num / exp;
+			_putchar(digit + '0');
+			num = num - (digit * exp);
+			exp = exp / 10;
+			i++;
+		}
+	}
+	_putchar(last + '0');
+
+	return (i);
+}
+
+file: _printf.c
+
+#include "main.h"
+/**
+ * _printf - is a function that selects the correct function to print.
+ * @format: identifier to look for.
+ * Return: the length of the string.
+ */
+int _printf(const char * const format, ...)
+{
+	convert p[] = {
+		{"%s", print_s}, {"%c", print_c},
+		{"%%", print_37},
+		{"%i", print_i}, {"%d", print_d}, {"%r", print_revs},
+		{"%R", print_rot13}, {"%b", print_bin},
+		{"%u", print_unsigned},
+		{"%o", print_oct}, {"%x", print_hex}, {"%X", print_HEX},
+		{"%S", print_exc_string}, {"%p", print_pointer}
+	};
+
+	va_list args;
+	int i = 0, j, length = 0;
+
+	va_start(args, format);
+	if (format == NULL || (format[0] == '%' && format[1] == '\0'))
+		return (-1);
+
+Here:
+	while (format[i] != '\0')
+	{
+		j = 13;
+		while (j >= 0)
+		{
+			if (p[j].ph[0] == format[i] && p[j].ph[1] == format[i + 1])
+			{
+				length += p[j].function(args);
+				i = i + 2;
+				goto Here;
+			}
+			j--;
+		}
+		_putchar(format[i]);
+		length++;
+		i++;
+	}
+	va_end(args);
+	return (length);
+}
